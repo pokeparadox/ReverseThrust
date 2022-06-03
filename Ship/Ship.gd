2e8636fa10c4d15e52507e54d1a22aa12e03b74d
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2022-06-03 19:29:13 +0100

    Added death wall and floor and sound effects
    -Using SFXR addonfor sound effect generaton
    - Added explosion and thrust sounds.
    - Added kill floor and walls with reset

--- Ship/Ship.gd
+++ Ship/Ship.gd
@@ -9,7 +9,6 @@ var shipHeading : Brad
 const ROTATE_SPEED : int = 200
 const THRUST_SPEED : int = 20
 
-const FLOOR_NORMAL = Vector2.UP
 
 var _velocity = Vector2.ZERO
 
@@ -17,7 +16,11 @@ func _init() -> void:
 	shipHeading = Brad.new()
 
 func _ready() -> void:
+	_velocity = Vector2.ZERO
 	self.position = screenRes * 0.5
+	shipHeading.SetAngle(0)
+	$ThrustSound.stop()
+
 
 func _physics_process(delta):
 	var rot = -(Input.get_action_strength("RotateLeft") - Input.get_action_strength("RotateRight"))
@@ -25,11 +28,21 @@ func _physics_process(delta):
 	SetShipThrusterAngle(shipHeading.GetAngleDeg())
 
 	if Input.is_action_pressed("Thrust"):
+		if not $ThrustSound.is_playing():
+			$ThrustSound.play()
+
 		var vec : Vector2 = BradLut.BradToVector2d(shipHeading) * delta * THRUST_SPEED
 		vec.y = -vec.y
 		_velocity += vec
+	else:
+		$ThrustSound.stop()
+
 	_velocity.y += gravity * delta * 0.01
-	self.position += _velocity
+	var _collision = move_and_collide(_velocity)
+	if _collision:
+		$ExplodeSound.play()
+		_ready()
+
 
 
 func SetShipThrusterAngle(thrust: float):
