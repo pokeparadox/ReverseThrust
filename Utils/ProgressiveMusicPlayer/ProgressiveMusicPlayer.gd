410c37b6f257e8b46712b802f4b21cd81ad71056
pokeparadox <pokeparadox@gmail.com>
2022-12-30 14:00:01 +0000

    Added music intensity progression
    When the last loop is played the looping will reset to loop 3 and
    increase the pitch 5%
    This is cummulative so will continue until the player eventually dies.

--- Utils/ProgressiveMusicPlayer/ProgressiveMusicPlayer.gd
+++ Utils/ProgressiveMusicPlayer/ProgressiveMusicPlayer.gd
@@ -3,21 +3,35 @@ extends Node2D
 export var ApproxLoopLength : float = 0.0
 export var ExactLoopLength : float = 0.0
 export var LoopOffsetCount : int = 0
+
 var LoopStart : float = 0.0
 var LoopEnd : float = 0.0
 var NumSections : int = 0
+
 
 func SetExactLoopLength() -> void:
 	var fullLength : float = $Track1.get_stream().get_length()
 	NumSections = int(fullLength/ApproxLoopLength)
 	ExactLoopLength = fullLength / float(NumSections)
 
-func Play() -> void:
+
+func reset_pitch() -> void:
+	$Track1.pitch_scale = 1
+
+
+func play(loop_offset : int = -1) -> void:
 	SetExactLoopLength()
-	$Track1.play()
-	LoopOffsetCount = 0
-	LoopStart = 0.0
-	LoopEnd = ExactLoopLength
+	if loop_offset == -1:
+		$Track1.play()
+		LoopOffsetCount = 0
+		LoopStart = 0.0
+		LoopEnd = ExactLoopLength
+	else:
+		LoopOffsetCount = loop_offset
+		LoopStart = ExactLoopLength * loop_offset
+		LoopEnd = LoopStart + ExactLoopLength
+		$Track1.play(LoopStart)
+
 
 func PlayPause() -> void:
 	if $Track1.isPlaying:
@@ -25,24 +39,20 @@ func PlayPause() -> void:
 	else:
 		$Track1.play()
 
+
 func Stop() -> void:
 	$Track1.stop()
 
+
 func SkipLoop() -> void:
 	if ExactLoopLength > 0 and LoopOffsetCount > 0:
-		var p = $Track1
-		var position : float = p.get_playback_position()
-		var newPos : float = position + ExactLoopLength
-		if newPos > p.get_stream().get_length():
-			return
-		#p.play(position + ExactLoopLength)
 		var newLoopStart : float = LoopOffsetCount * ExactLoopLength
-		#LoopStart = LoopOffsetCount * ExactLoopLength
 		LoopEnd = newLoopStart + ExactLoopLength
 	else:
-		Play()
+		play()
 	if ExactLoopLength > 0:
 		LoopOffsetCount += 1
+
 
 func _process(_delta: float) -> void:
 	if ExactLoopLength > 0:
@@ -51,6 +61,13 @@ func _process(_delta: float) -> void:
 		if position > LoopEnd:
 			LoopStart = LoopOffsetCount * ExactLoopLength
 			p.play(LoopStart)
-			if LoopStart == LoopEnd:
-				LoopEnd = LoopEnd + ExactLoopLength
+			if LoopStart >= LoopEnd:
+				LoopEnd = LoopStart + ExactLoopLength
 
+
+func _on_Track1_finished():
+	var p = $Track1
+	p.pitch_scale += 0.05
+	play(3)
+	
+	
